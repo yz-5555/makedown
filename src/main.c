5e7670670f1439d5d8daeb9772c73da49535cc4c
yz <yunjae00734@gmail.com>
2024-12-22 02:19:57 +0900

    (feat) raylib

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
-#include "makedown.h"
+#include <raylib.h>
 
 int main(void)
 {
-	parse_markdown_line("hoho");
+	InitWindow(800, 600, "Hello, Raylib!");
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		{
+			ClearBackground(WHITE);
+			DrawText("Hello, Raylib!", 100, 100, 20, LIGHTGRAY);
+		}
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
 }
